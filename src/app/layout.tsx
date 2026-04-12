3695d6d7e2e208f413b6b02b37da73d464f7f044
TrezeguetSanteb <strezeguet@fi.uba.ar>
2026-04-12 20:23:55 -0300

    logo en el buscador de google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,6 +68,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: "/icon.png", sizes: "512x512", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon.ico", sizes: "any" },
     ],
